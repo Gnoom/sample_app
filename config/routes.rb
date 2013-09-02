715a12fea5865fc104b4b7b395a350433f913d0e
Gnoom <tgnoom@gmail.com>
2013-09-03 01:44:32 +0900

    new

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ SampleApp::Application.routes.draw do
   match '/contact', to: 'static_pages#contact', via: 'get'
   
 
-  # The priority is based upon order of creation: first created -> highest priority.
+  # test The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
